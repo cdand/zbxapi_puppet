98c0ae5ba95531336e6ffe561f159117077c8ef9
Chuck Dand <chuckd@kooked.co.uk>
2013-03-25 12:59:27 +0800

    add puppet_x for shared libraries - currently unused

---
+++ lib/puppet_x/helper.rb
@@ -0,0 +1,12 @@
+#require "zbxapi"
+module Zabbix
+
+ZABBIX_URL='http://localhost/'
+ZABBIX_USER='admin'
+ZABBIX_PASSWD='zabbix'
+
+@zabbix = ZabbixAPI::ZabbixAPI.new(ZABBIX_URL)
+@zabbix.verify_ssl = false
+@zabbix.login(ZABBIX_USER, ZABBIX_PASSWD)
+
+end
