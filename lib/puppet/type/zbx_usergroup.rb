7c64ac576601afd1ff7de659e179db10ad729173
Chuck Dand <chuckd@kooked.co.uk>
2013-03-27 16:52:23 +0800

    add input munging for human readable settings in zbx_usergroup

--- lib/puppet/type/zbx_usergroup.rb
+++ lib/puppet/type/zbx_usergroup.rb
@@ -21,17 +21,56 @@ Puppet::Type.newtype(:zbx_usergroup) do
 			internal = Internal authentication only
 			disabled = No Web GUI access
 	  EOT
-		defaultto 0
+		defaultto :system
+		newvalues( :system, :internal, :disabled )
+		munge do |value|
+			case value
+			when "system"
+				0
+			when "internal"
+				1
+			when "disabled"
+				2
+			end
+		end
 	end
 
 	newproperty(:enabled) do
-		desc 'Is this Usergroup enabled'
-		defaultto 0
+		desc <<-EOT
+      Is this Usergroup enabled.
+
+			true  = Member of this Usergroup can login
+			false = Members of this usergroup cannot login
+		EOT
+		defaultto :true
+		newvalues( :true, :false )
+		munge do |value|
+			case value
+			when "true"
+				0
+			when "false"
+				1
+			end
+		end
 	end
 
 	newproperty(:debug) do
-		desc 'Is Debug mode enabled for members of this Usergroup'
-		defaultto 0
+		desc <<-EOT
+      Is Debug mode enabled for members of this Usergroup'
+
+			true
+			false
+		EOT
+		defaultto :false
+		newvalues( :true, :false )
+		munge do |value|
+			case value
+			when "true"
+				1
+			when "false"
+				0
+			end
+		end
 	end
 
 end
