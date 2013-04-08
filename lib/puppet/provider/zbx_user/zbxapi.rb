a8ca44cc8663fca8cef802161b2d994d94d42528
Chuck Dand <chuckd@kooked.co.uk>
2013-04-08 14:14:06 +0800

    add flush and some more property control to zbx_user provider

--- lib/puppet/provider/zbx_user/zbxapi.rb
+++ lib/puppet/provider/zbx_user/zbxapi.rb
@@ -14,13 +14,14 @@ Puppet::Type.type(:zbx_user).provide(:zbxapi) do
   def self.instances
     users = $zabbix.user.get( 'output' => 'extend' )
     users.collect do |user|
-			new( :name       => user["alias"],
-          :ensure      => :present,
-          :userid      => user["userid"],
-          :firstname   => user["name"],
-          :surname     => user["surname"],
-          :usertype    => user["type"],
-         )
+    new( :name      => user["alias"],
+         :ensure    => :present,
+         :userid    => user["userid"],
+         :usergroup => user["usrgrpid"],
+         :firstname => user["name"],
+         :surname   => user["surname"],
+         :usertype  => user["type"],
+       )
     end
   end
 
@@ -38,8 +39,16 @@ Puppet::Type.type(:zbx_user).provide(:zbxapi) do
   end
 
   def create
-    $zabbix.user.create( 'alias' => resource[:name], 'passwd' => 'blah', usrgrps:[{'usrgrpid' => '7'}] )
+    $zabbix.user.create(
+      'alias' => resource[:name],
+      'type' => resource[:usertype],
+      'name' => resource[:firstname],
+      'surname' => resource[:surname],
+      'passwd' => 'blah',
+      'usrgrps' => [{'usrgrpid' => '7'}]
+    )
     @property_hash[:ensure] = :present
+    @property_hash[:usertype] = resource[:usertype]
   end
 
   def destroy
@@ -47,6 +56,41 @@ Puppet::Type.type(:zbx_user).provide(:zbxapi) do
     @property_hash.clear
   end
 
-	mk_resource_methods
+  # Using mk_resource_methods relieves us from having to explicitly write the
+  # getters for all properties. I'm not sure yet how this can possibly work
+	# for setters though.
+  mk_resource_methods
+
+  # Despite Puppet Labs docs, it would appear mk_resource methods is only
+  # useful for getters. We need to create all the different setters.
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+  end
+
+  def userid=(value)
+    @property_flush['userid'] = value
+  end
+
+  def usertype=(value)
+    @property_flush['type'] = value
+  end
+
+  def firstname=(value)
+    @property_flush['name'] = value
+  end
+
+  def surname=(value)
+    @property_flush['surname'] = value
+  end
+
+  def flush
+    unless @property_flush.empty?
+			@property_flush['userid'] = @property_hash[:userid]
+      $zabbix.user.update( @property_flush )
+    end
+    @property_hash = resource.to_hash
+  end
+
 
 end
