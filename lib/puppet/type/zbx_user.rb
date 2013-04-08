a53d72c0baf8205f50df48ffdbde77f03cbc8504
Chuck Dand <chuckd@kooked.co.uk>
2013-04-08 14:09:35 +0800

    add usertype defaults and munging to zbx_user type

--- lib/puppet/type/zbx_user.rb
+++ lib/puppet/type/zbx_user.rb
@@ -17,11 +17,31 @@ Puppet::Type.newtype(:zbx_user) do
   end
 
   newproperty(:surname) do
-    desc 'The firstname of the Zabbix User'
+    desc 'The surname of the Zabbix User'
   end
 
   newproperty(:usertype) do
-    desc 'The firstname of the Zabbix User'
+    desc 'The usertype of the Zabbix User'
+    defaultto :user
+    newvalues( :user, :admin, :super )
+    munge do |value|
+      case value
+      when "user"
+        1
+      when "admin"
+        2
+      when "super"
+        3
+      end
+    end
+  end
+
+  newproperty(:usergroup) do
+    desc 'The usergroup of the Zabbix User'
+  end
+
+  autorequire(:usergroup) do
+    self[:usergroup]
   end
 
 end
