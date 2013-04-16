e94fa0906b2f2daa8ead3bf94084c1266361c68a
Chuck Dand <chuckd@kooked.co.uk>
2013-04-16 20:10:47 +0800

    Make zbx_templates_item more functional - it works I'd say

--- lib/puppet/provider/zbx_template_item/zbxapi.rb
+++ lib/puppet/provider/zbx_template_item/zbxapi.rb
@@ -3,12 +3,19 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'pu
 Puppet::Type.type(:zbx_template_item).provide(:zbxapi) do
 
   def self.instances
+    # We pull in a list of all templates here so that we can set the value of #
+    #@property_flush[:template] to satisfy Puppet's desire to change/set this.  #
+    #This add computational complexity, but saves having thousands of API calls
+    #later on.
+    templates = $zabbix.template.get( 'output' => 'extend' )
     items = $zabbix.item.get( 'output' => 'extend', 'templated' => true, 'inherited' => false )
     items.collect do |item|
+      template = templates.detect { |template| template["hostid"] == item["hostid"] }["host"]
       new( :name        => item["name"],
            :ensure      => :present,
            :itemid      => item["itemid"],
            :hostid      => item["hostid"],
+           :template    => template,
            :delay       => item["delay"],
            :key_        => item["key_"],
            :interfaceid => item["interfaceid"],
@@ -59,6 +66,31 @@ Puppet::Type.type(:zbx_template_item).provide(:zbxapi) do
   def initialize(value={})
     super(value)
     @property_flush = {}
+  end
+
+  def delay=(value)
+    @property_flush['delay'] = value
+  end
+
+  def key_=(value)
+    @property_flush['key_'] = value
+  end
+
+  def interfaceid=(value)
+    @property_flush['interfaceid'] = value
+  end
+
+  def value_type=(value)
+    @property_flush['value_type'] = value
+  end
+
+  def type=(value)
+    @property_flush['type'] = value
+  end
+
+  def template=(value)
+    templateid = $zabbix.template.get( 'output' => 'shorten', 'filter' => { 'name' => value } ).first["templateid"]
+    @property_flush['template'] = templateid
   end
 
   def flush
