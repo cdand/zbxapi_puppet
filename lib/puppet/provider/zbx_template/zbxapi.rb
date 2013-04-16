230b09416157d852aefe5454dbafc183f637975b
Chuck Dand <chuckd@kooked.co.uk>
2013-04-16 15:34:44 +0800

    Update to largely functional zbx_template type and provider

--- lib/puppet/provider/zbx_template/zbxapi.rb
+++ lib/puppet/provider/zbx_template/zbxapi.rb
@@ -3,11 +3,22 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'pu
 Puppet::Type.type(:zbx_template).provide(:zbxapi) do
 
   def self.instances
-    templates = $zabbix.template.get( 'output' => 'extend' )
+    templates = $zabbix.template.get( 'output' => 'extend', 'selectGroups' => 'extend', 'selectParentTemplates' => 'extend' )
     templates.collect do |template|
+      hostgroups = []
+      template['groups'].each do |group|
+        hostgroups << group['name']
+      end
+      parents = []
+      template['parentTemplates'].each do |parent|
+        parents << parent['name']
+      end
       new( :name        => template["host"],
            :ensure      => :present,
+           :templateid  => template["templateid"],
            :visiblename => template["name"],
+           :hostgroups  => hostgroups,
+           :parents     => parents,
       )
     end
   end
@@ -26,7 +37,16 @@ Puppet::Type.type(:zbx_template).provide(:zbxapi) do
   end
 
   def create
-    $zabbix.template.create( 'name' => resource[:name] )
+		hostgroupids = $zabbix.hostgroup.get( 'output' => 'shorten', 'filter' => {'name' => resource[:hostgroups]})
+		templateids = []
+		unless resource[:parents].empty?
+  		templateids  = $zabbix.template.get( 'output' => 'shorten', 'filter' => { 'host' => resource[:parents] })
+		end
+    $zabbix.template.create( 'host'      => resource[:name],
+                             'name'      => resource[:visiblename],
+                             'groups'    => hostgroupids,
+                             'templates' => templateids,
+                             'hosts'     => [] )
     @property_hash[:ensure] = :present
   end
 
@@ -47,8 +67,26 @@ Puppet::Type.type(:zbx_template).provide(:zbxapi) do
     @property_flush = {}
   end
 
-  def templateid=(value)
-    @property_flush['templateid'] = value
+  def visiblename=(value)
+    #FIXME If a visiblename is set and then removed Puppet doesn't remove the visible name from Zabbix configuration.
+    @property_flush['name'] = value
+  end
+
+  def hostgroups=(value)
+    #FIXME There is some issue with the ordering of hostgroupids array, if not in hostgroup id numerical order Puppet keeps trying to make a change.
+    # Can't quite figure out where this is happening.
+    hostgroupids = $zabbix.hostgroup.get( 'output' => 'shorten', 'filter' => {'name' => value})
+    @property_flush['groups'] = hostgroupids
+  end
+
+  def parents=(value)
+    #FIXME There is some issue with the ordering of templateids array, if not in template id numerical order Puppet keeps trying to make a change.
+    # Can't quite figure out where this is happening.
+    templateids = []
+    unless value.empty?
+      templateids  = $zabbix.template.get( 'output' => 'shorten', 'filter' => { 'host' => value })
+    end
+    @property_flush['templates'] = templateids
   end
 
   def flush
--- lib/puppet/type/zbx_template.rb
+++ lib/puppet/type/zbx_template.rb
@@ -9,7 +9,31 @@ Puppet::Type.newtype(:zbx_template) do
   end
 
 	newproperty(:visiblename) do
+		#FIXME If a visiblename is set and then removed Puppet doesn't remove the visible name from Zabbix configuration.
+		#      isn't as simple as just setting a blank default.
     desc 'The visible name of the Zabbix Template'
   end
+
+  newproperty(:hostgroups, :array_matching => :all) do
+    desc <<-EOT
+      Array of Hostgroups to add the template to.
+			
+			(default) ['Templates']
+
+			e.g. ['Templates', 'Linux templates']
+    EOT
+		defaultto ['Templates']
+  end
+
+  newproperty(:parents, :array_matching => :all) do
+    desc <<-EOT
+      Array of parent templates to link the template to.
+			
+			(default) [] = empty array
+
+			e.g. ['Template Ruckus Controller', 'Templates SNMP Generic']
+    EOT
+ 		defaultto []
+ end
 
 end
