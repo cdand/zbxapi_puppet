372e51c733da1b95d29fa489ef9933425c81b8ff
Chuck Dand <chuckd@kooked.co.uk>
2013-04-17 01:05:29 +0800

    change some comments in zbx_template_item provider

--- lib/puppet/provider/zbx_template_item/zbxapi.rb
+++ lib/puppet/provider/zbx_template_item/zbxapi.rb
@@ -3,8 +3,8 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'pu
 Puppet::Type.type(:zbx_template_item).provide(:zbxapi) do
 
   def self.instances
-    # We pull in a list of all templates here so that we can set the value of #
-    #@property_flush[:template] to satisfy Puppet's desire to change/set this.  #
+    # We pull in a list of all templates here so that we can set the value of
+    #@property_flush[:template] to satisfy Puppet's desire to change/set this.
     #This add computational complexity, but saves having thousands of API calls
     #later on.
     templates = $zabbix.template.get( 'output' => 'extend' )
