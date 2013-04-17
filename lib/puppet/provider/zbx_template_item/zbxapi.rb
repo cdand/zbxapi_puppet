dab1369970bc7d6bc4720c3fe8f16a08f55733ef
Chuck Dand <chuckd@kooked.co.uk>
2013-04-18 00:13:22 +0800

    add lots of property setters to zbx_template_item provider

--- lib/puppet/provider/zbx_template_item/zbxapi.rb
+++ lib/puppet/provider/zbx_template_item/zbxapi.rb
@@ -93,6 +93,142 @@ Puppet::Type.type(:zbx_template_item).provide(:zbxapi) do
     @property_flush['template'] = templateid
   end
 
+  def authtype=(value)
+    @property_flush['authtype'] = value
+  end
+
+  def data_type=(value)
+    @property_flush['data_type'] = value
+  end
+
+  def delay_flex=(value)
+    @property_flush['delay_flex'] = value
+  end
+
+  def delta=(value)
+    @property_flush['delta'] = value
+  end
+
+  def description=(value)
+    @property_flush['description'] = value
+  end
+
+  def error=(value)
+    @property_flush['error'] = value
+  end
+
+  def flags=(value)
+    @property_flush['flags'] = value
+  end
+
+  def formula=(value)
+    @property_flush['formula'] = value
+  end
+
+  def history=(value)
+    @property_flush['history'] = value
+  end
+
+  def inventory_link=(value)
+    @property_flush['inventory_link'] = value
+  end
+
+  def ipmi_sensor=(value)
+    @property_flush['ipmi_sensor'] = value
+  end
+
+  def lastclock=(value)
+    @property_flush['lastclock'] = value
+  end
+
+  def lastns=(value)
+    @property_flush['lastns'] = value
+  end
+
+  def logtimefmt=(value)
+    @property_flush['logtimefmt'] = value
+  end
+
+  def mtime=(value)
+    @property_flush['mtime'] = value
+  end
+
+  def multiplier=(value)
+    @property_flush['multiplier'] = value
+  end
+
+  def params=(value)
+    @property_flush['params'] = value
+  end
+
+  def password=(value)
+    @property_flush['password'] = value
+  end
+
+  def port=(value)
+    @property_flush['port'] = value
+  end
+
+  def privatekey=(value)
+    @property_flush['privatekey'] = value
+  end
+
+  def publickey=(value)
+    @property_flush['publickey'] = value
+  end
+
+  def snmp_community=(value)
+    @property_flush['snmp_community'] = value
+  end
+
+  def snmp_oid=(value)
+    @property_flush['snmp_oid'] = value
+  end
+
+  def snmpv3_authpassphrase=(value)
+    @property_flush['snmpv3_authpassphrase'] = value
+  end
+
+  def snmpv3_privpassphrase=(value)
+    @property_flush['snmpv3_privpassphrase'] = value
+  end
+
+  def snmpv3_securitylevel=(value)
+    @property_flush['snmpv3_securitylevel'] = value
+  end
+
+  def snmpv3_securityname=(value)
+    @property_flush['snmpv3_securityname'] = value
+  end
+
+  def enabled=(value)
+    @property_flush['enabled'] = value
+  end
+
+  def templateid=(value)
+    @property_flush['templateid'] = value
+  end
+
+  def trapper_hosts=(value)
+    @property_flush['trapper_hosts'] = value
+  end
+
+  def trends=(value)
+    @property_flush['trends'] = value
+  end
+
+  def units=(value)
+    @property_flush['units'] = value
+  end
+
+  def username=(value)
+    @property_flush['username'] = value
+  end
+
+  def valuemapid=(value)
+    @property_flush['valuemapid'] = value
+  end
+
   def flush
     unless @property_flush.empty?
 			@property_flush['itemid'] = @property_hash[:itemid]
